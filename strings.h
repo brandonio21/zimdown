5638c7d2a30541e71f86000c9fc4df30e010c3fa
Brandon Milton <brandon.milton21@gmail.com>
2014-05-19 15:36:38 -0700

    Added strings file that contains error messages, etc.

---
+++ strings.h
@@ -0,0 +1,23 @@
+/******************************************************************************
+* File:    strings.h
+* Author:  Brandon Milton
+* Website: http://brandonsoft.com
+* GitHub:  http://github.com/brandonio21
+* May 19, 2014
+*
+* The purpose of strings.h is to contain all strings that zimdown.c will output
+* to the user, so that they can be easily changed just in case there are 
+* some mistakes. This is in good C style, I reckon.
+******************************************************************************/
+#ifndef ZIMDOWN_STRINGS
+#define ZIMDOWN_STRINGS
+
+#define STR_USAGE "l"
+
+#define STR_ERR_READFILE "Error reading file"
+#define STR_ERR_WRITEFILE "Error opening file for writing"
+
+
+
+
+#endif
